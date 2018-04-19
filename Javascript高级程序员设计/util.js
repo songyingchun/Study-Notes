045e25749d49ca4fe69a438322486e3de2c6886f
songyingchun <419973879@qq.com>
2018-04-19 19:04:39 +0800

    ajax

--- Javascript高级程序员设计/util.js
+++ Javascript高级程序员设计/util.js
@@ -277,7 +277,7 @@ function setInnerText(element, text) {
 	}
 }
 
-var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+// var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 
 function insertRule(sheet, selectorText, cssText, position) {
 	if (sheet.insertRule) {
@@ -623,3 +623,27 @@ var EventUtil = {
 		return parts.join("&");
 	}
 };
+
+function createXHR() {
+	if (typeof XMLHttpRequest != "undefined") {
+		return new XMLHttpRequest();
+	} else if (typeof ActiveXObject != "undefined") {
+		if (typeof arguments.callee.activeXString != "string") {
+			var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0",
+				"MSXML2.XMLHttp"],
+				i, len;
+			for (i = 0, len = versions.length; i < len; i++) {
+				try {
+					new ActiveXObject(versions[i]);
+					arguments.callee.activeXString = versions[i];
+					break;
+				} catch (ex) {
+					//跳过
+				}
+			}
+		}
+		return new ActiveXObject(arguments.callee.activeXString);
+	} else {
+		throw new Error("No XHR object available.");
+	}
+}
