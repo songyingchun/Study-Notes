4f95d1f6040b726a4396990a1310aed58737ac38
songyingchun <419973879@qq.com>
2018-10-28 11:40:45 +0800

    d

---
+++ 全栈开发/05generator/module/seajs/mod1.js
@@ -0,0 +1,14 @@
+define(function (require, exports, module) {
+  //exports
+  exports.a = 12;
+  exports.b = 5;
+  //module-批量导出
+  module.exports={
+    a:12,
+    b:5
+  };
+  // require
+  
+  // let a = 12;
+  // let b = 5;
+});
---
+++ 全栈开发/05generator/module/seajs/mod2.js
@@ -0,0 +1,5 @@
+define(function (require, exports, module) {
+  let moda = require('a');
+  let modb = require('b');
+  exports.res = moda.num1 + modb.num2;
+});
